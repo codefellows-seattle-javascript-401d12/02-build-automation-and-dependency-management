6796ddbf14311771b89beeda98f7a732ee469746
Steven Bateman <batemanvo@gmail.com>
2016-12-06 20:23:36 -0800

    Refactored tests to use chai's expect method.

--- lab-steven/test/test.js
+++ lab-steven/test/test.js
@@ -1,17 +1,20 @@
 'use strict';
 
 const greet = require('../lib/greet.js');
-const assert = require('assert');
+const expect = require('chai').expect;
 
 describe('Greet', function() {
+  describe('#getName', function() {
+    it('Should have a name equal to Mocha if no name provided in CLI.', function() {
+      var testName = greet.getName();
+      expect(testName).to.equal('Mocha');
+    });
+  });
+
   describe('#greet', function() {
     it('Expected to output "hello <name>"', function() {
       var result = greet.greet(process.argv[2]);
-      assert.ok(result === `hello ${process.argv[2]}`, `Instead says ${result}`);
-    });
-
-    it('Name expected to be undefined when using mocha', function() {
-      assert.ok(greet.greet() === 'hello undefined', 'Did not come back as hello undefined');
+      expect(result).to.equal('hello undefined');
     });
   });
 });

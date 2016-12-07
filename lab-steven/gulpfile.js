b577648c1eb8d3bae8075c6fe5a8c5fd10501cb2
Steven Bateman <batemanvo@gmail.com>
2016-12-06 20:37:39 -0800

    Filled in test and lint tasks in gulpfile.js.

--- lab-steven/gulpfile.js
+++ lab-steven/gulpfile.js
@@ -7,3 +7,16 @@ const mocha = require('gulp-mocha');
 gulp.task('test', function() {
   gulp.src('./test/*-test.js', {read: false}).pipe(mocha({reporter: 'nyan'}));
 });
+
+gulp.task('lint', function() {
+  return gulp.src(['**/*.js', '!node_modules/**'])
+  .pipe(linter())
+  .pipe(linter.format())
+  .pipe(linter.failAfterError());
+});
+
+gulp.task('dev', function() {
+  gulp.watch(['**/*.js', '!node_modules/**'], ['lint', 'test']);
+});
+
+gulp.task('default', ['dev']);

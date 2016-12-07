50311658b8cadeefb1bbd8715d9d2bdbca9ff179
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-07 09:48:24 -0800

    build out gulp test automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+
+// this task automates our tests.
+// it looks for any test scripts in our test dir, and following our file naming convention.
+// it also 'pipes in' and utilizes mocha.
+gulp.task('test', function() {
+  gulp.src('./test/*-test.js', { read: false })
+  .pipe(mocha({ reporter: 'spec' }));
+});

f375fcd8bc0ce714e2296d12c162f7b006b1383c
Kyler Dotson <kyler.dotson@gmail.com>
2016-12-07 10:17:23 -0800

    changed folder name oops

--- lab-kyler2/greet.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-module.exports = {};
-
-if (process.argv[2]) console.log('Hello, ' + process.argv[2] + '!');
-
-module.exports.greet = function(name) {
-  if (!name) throw new Error('No name provided!');
-  return 'Hello, ' + name + '!';
-};

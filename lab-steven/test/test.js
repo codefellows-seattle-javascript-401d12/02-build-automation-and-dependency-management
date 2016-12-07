f296c66e8c805058300c1de9d113cb6d527a1da1
Steven Bateman <batemanvo@gmail.com>
2016-12-06 20:29:32 -0800

    Refactored test to only call greet.getName once.

--- lab-steven/test/test.js
+++ lab-steven/test/test.js
@@ -4,17 +4,17 @@ const greet = require('../lib/greet.js');
 const expect = require('chai').expect;
 
 describe('Greet', function() {
+  const testName = greet.getName();
   describe('#getName', function() {
     it('Should have a name equal to Mocha if no name provided in CLI.', function() {
-      var testName = greet.getName();
       expect(testName).to.equal('Mocha');
     });
   });
 
   describe('#greet', function() {
     it('Expected to output "hello <name>"', function() {
-      var result = greet.greet(process.argv[2]);
-      expect(result).to.equal('hello undefined');
+      const result = greet.greet(testName);
+      expect(result).to.equal('hello Mocha');
     });
   });
 });

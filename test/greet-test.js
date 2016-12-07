9c2613d884b60c67d3e7b6a1e06af0d7771d11be
Jonathan Heemstra <jonathan.heemstra@gmail.com>
2016-12-07 10:03:55 -0800

    fix tests to use name variable

--- test/greet-test.js
+++ test/greet-test.js
@@ -7,11 +7,15 @@ let name = process.argv[2];
 if(!name) name = 'jonny';
 
 describe('Test greet file', function() {
-  describe('#sayHey', function(name) {
+  describe('#sayHey', function() {
+    it('should have a method sayHey', function() {
+      expect(greet).to.have.property('sayHey');
+      expect(greet).to.respondTo('sayHey');
+    });
     it(`should return what\'s up ${name}`, function() {
-      let name = greet.sayHey('jonny');
-      expect(name).to.be.a('string');
-      expect(name).to.equal('what\'s up jonny!');
+      let greeting = greet.sayHey(name);
+      expect(greeting).to.be.a('string');
+      expect(greeting).to.equal(`what\'s up ${name}!`);
     });
     it('should return missing name error', function() {
       let name = greet.sayHey;
@@ -19,10 +23,14 @@ describe('Test greet file', function() {
     });
   });
   describe('#sayBye', function() {
+    it('should have a method sayBye', function() {
+      expect(greet).to.have.property('sayBye');
+      expect(greet).to.respondTo('sayBye');
+    });
     it(`should return see ya later ${name}`, function() {
-      let name = greet.sayBye('jonny');
-      expect(name).to.be.a('string');
-      expect(name).to.equal('See ya later jonny!');
+      let greeting = greet.sayBye(name);
+      expect(greeting).to.be.a('string');
+      expect(greeting).to.equal(`See ya later ${name}!`);
     });
     it('should return missing name error', function(){
       let name = greet.sayBye;

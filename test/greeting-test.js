a5ca76d3dac0f6785c4c2daa5597a659993f6422
Caleb Sattgast <calebsattgast@gmail.com>
2016-12-07 09:37:15 -0800

    refactor tests to use chai expect statements

--- test/greeting-test.js
+++ test/greeting-test.js
@@ -2,18 +2,18 @@
 
 const greeting = require('../lib/greeting.js');
 
-const assert = require('assert');
+const expect = require('chai').expect;
 
 describe('Greeting', function() {
   describe('#welcome', function() {
-    it('should print a greeting message customized with the user\'s inputted name.', function() {
+    it('should print a greeting message customized with the name \'Caleb.\'', function() {
       var result = greeting.welcome('Caleb');
-      assert.ok(result === 'Hello and welcome, Caleb! Glad you can be here!', 'greeting not printed as expected');
+      expect(greeting).to.have.property('welcome');
+      expect(result).to.equal('Hello and welcome, Caleb! Glad you can be here!', 'greeting not printed as expected');
     });
     it('should throw an error indicating a missing name or unexpected charater', function() {
-      assert.throws(function() {
-        greeting.welcome();
-      }, 'error not thrown');
+      var result = greeting.welcome;
+      expect(result).to.throw(Error);
     });
   });
 });

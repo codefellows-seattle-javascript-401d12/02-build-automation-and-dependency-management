5b87335d699cdb819556aa572f6c075f074ab32a
kcirekcom <erick.f.mock@gmail.com>
2016-12-06 17:32:30 -0800

    invoked eslint.failAfterError in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('lint', function () {
   return gulp.src(['**/*.js', '!node_modules'])
   .pipe(eslint())
   .pipe(eslint.format())
-  .pipe(eslint.failAfterError);
+  .pipe(eslint.failAfterError());
 });
 
 gulp.task('dev', function () {

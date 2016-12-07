34496eb27bce34653435bc7e110b4fc9fef3f36a
Hawa Abdi <abdih17@hotmail.com>
2016-12-07 00:48:57 -0800

    Ran tests

--- tests/test.js
+++ tests/test.js
@@ -7,10 +7,11 @@ var name = process.argv[2];
 describe('Greet Module', function() {
   describe('#sayHi', function() {
     it('should return Hi {name}!', function() {
-      var result = greet.sayHi(name);
+      var result = greet.sayHi(process.argv[2]);
       //expect(greet) is invoking  greet.sayHi(name)
+      expect(result).to.equal('hi ${name}!');
+      expect(result).to.not.equal(parseInt);
       expect(greet).to.have.property('sayHi');
-      expect(result).to.equal('hi hawa');
     });
     it('should throw a missing name error', function() {
       var result = greet.sayHi;
@@ -24,6 +25,7 @@ describe('Greet Module', function() {
     it('should return See ya later {petName}!', function(){
       var result = greet.saySeeYaLater('Gaara');
       expect(result).to.equal('See ya later, Gaara!');
+      // expect.to.have.property('saySeeYaLater');
     });
     it('should throw a missing name error', function(){
       //TODO: finish building out test
